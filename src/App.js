8b8dd9d10cd32984b1fa05ce9efed98d26976ce4
Ahmed-Silat <ahmedsilat95@gmail.com>
2022-06-24 10:57:46 +0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [list, setList] = useState([])
+  const [text, setText] = useState('')
+  const [editMode, setEditMode] = useState(false)
+  const [editIndex, setEditIndex] = useState()
+
+  const addItem = () => {
+    if (!text) {
+      alert("Input Text")
+      return
+    }
+
+    const tempList = [...list]
+    tempList.push(text)
+    setList(tempList)
+
+    resetState()
+  }
+
+  const deleteItem = (index) => {
+    const tempList = [...list]
+    tempList.splice(index, 1)
+    setList(tempList)
+
+    resetState()
+  }
+
+  const editItem = (index) => {
+    setText(list[index])
+    setEditMode(true)
+    setEditIndex(index)
+  }
+
+  const updateItem = () => {
+    const tempList = [...list]
+    tempList[editIndex] = text
+    setList(tempList)
+
+    setEditMode(false)
+    resetState()
+  }
+
+  const resetState = () => {
+    setText('')
+    setEditMode(false)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h2>Todo List</h2>
+
+        <input
+          placeholder='Enter item name'
+          onChange={e => setText(e.target.value)}
+          value={text}
+        />
+
+        {editMode ?
+          <button onClick={updateItem}>Update</button>
+          :
+          <button onClick={addItem}>Add</button>
+        }
+
+        <ul>
+          {list.map((item, index) => {
+            return <li>{item}
+              <button onClick={() => deleteItem(index)}>Delete</button>
+              <button onClick={() => editItem(index)}>Edit</button>
+            </li>
+          })}
+        </ul>
+
+        <br />
       </header>
     </div>
   );
